771801a2d81a280724d22f7e2a431738957262f0
Rocio Lopez Perez <rocio.nightwater@gmail.com>
2017-06-04 20:00:53 +0200

    Missing data approach - mean, LSTM model (ActFunc = relu), 1 layer, 16 cells. Bad predictions

---
+++ Algorithm.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from keras.models import Sequential
+import matplotlib.pyplot as plt
+from keras.layers import Dense, Dropout,Activation
+from keras.layers import Embedding
+from keras.layers import LSTM
+import numpy as np
+
+def lstm(x_train,y_train,x_test,y_test):
+    print(x_train.shape)
+    epochs = 1
+    batch_size = 50
+
+    model = Sequential()
+    layers = {'input': 16, 'hidden1': 64, 'output': 1}
+
+    model.add(LSTM(16,
+            input_shape=(None,16),
+            return_sequences=False))
+    #model.add(Dropout(0.2))
+
+    model.add(Dense(1))
+    model.add(Activation("relu"))
+
+    #start = time.time()
+    model.compile(loss="mse", optimizer="rmsprop")
+    #print "Compilation Time : ", time.time() - start
+    #return model
+    print("Training...")
+    model.fit(x_train, y_train,batch_size=batch_size, nb_epoch=epochs)
+    print("Predicting...")
+    predicted = model.predict(x_test)
+    predicted = np.reshape(predicted, (predicted.size,))
+
+    print (y_test)
+    print (predicted)
+
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # ax.plot(y_test[:93])
+    # plt.plot(predicted[:43])
+    # plt.show()
--- lstm.py
+++ lstm.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import scipy
+import Algorithm
 import numpy as np
 from scipy.io import arff
 from sklearn import preprocessing
@@ -17,6 +18,7 @@ cat_name   = 'categorized result'
 features = dataset[feature_names]
 labels   = dataset[label_name]
 categories = dataset[cat_name]
+
 
 #
 # If address is not 4, then it's always DoS attack in our dataset
@@ -69,18 +71,18 @@ imp = preprocessing.Imputer(missing_values='NaN', strategy=impute_strategy, axis
 packet_inspected_features = imp.fit_transform(packet_inspected_features)
 
 new_features = np.column_stack((addresses, lengths, crcs, timestamp_diffs, packet_inspected_features))
-print "Length: "+str(len(new_features))
+#print "Length: "+str(len(new_features))
 #Split the data - Training set and testing set
 X_train = new_features[:164777,:]
 X_crossValidation = new_features[164778:219704,:]
 X_test = new_features[219705:len(new_features)-1,:]
-print(new_features)
+#print(new_features)
 #print X_train
-print str(len(X_train))
+#print str(len(X_train))
 #print X_crossValidation
-print str(len(X_crossValidation))
+#print str(len(X_crossValidation))
 #print X_test
-print str(len(X_test))
+#print str(len(X_test))
 
 #Preprocess - Mean removal and variance scaling
 X_train_scaled = preprocessing.scale(X_train)
@@ -88,9 +90,44 @@ X_train_scaled = preprocessing.scale(X_train)
 scaler = preprocessing.StandardScaler().fit(X_train)
 X_crossValidation_scaled = scaler.transform(X_crossValidation)
 X_test_scaled = scaler.transform(X_test)         
-# After preprocessing, function feature is added (since this one didn't need to be preprocessed)
+# After preprocessing, function feature is added (since this one didn't need to be preprocessed )
 X_train_scaled = np.column_stack((X_train_scaled, functions[:164777]))
 X_crossValidation_scaled = np.column_stack((X_crossValidation_scaled,functions[164778:219704]))
 X_test_scaled = np.column_stack((X_test_scaled,functions[219705:len(new_features)-1]))
+
+y_train = labels[:164777]
+y_crossVal = labels[164778:219704]
+y_testData = labels[219705:len(new_features)-1]
+
+#Take only few samples
+X_train_scaled = X_train_scaled[:100]
+y_train = y_train[:100]
+X_crossValidation_scaled = X_crossValidation_scaled[101:151]
+y_crossVal = y_crossVal[101:151]
+
+sequence_length = 7
+X_train = []
+Y_train = []
+for index in range(len(X_train_scaled) - sequence_length):
+    X_train.append(X_train_scaled[index: index + sequence_length])
+    Y_train.append(y_train[index + sequence_length])
+X_train = np.array(X_train) 
+Y_train = np.array(Y_train)
+Y_train = Y_train[:len(X_train)]
+X_crossValidate = []
+Y_crossVal = []
+for index in range(len(X_crossValidation_scaled) - sequence_length):
+    X_crossValidate.append(X_crossValidation_scaled[index: index + sequence_length])
+    Y_crossVal.append(y_crossVal[index + sequence_length])
+X_crossValidate = np.array(X_crossValidate) 
+Y_crossVal = np.array(Y_crossVal)
+Y_crossVal = Y_crossVal[:len(X_crossValidate)]
+
+#Convert arrays in (#examples, #values in sequences, dim. of each value)
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 16))
+#X_train = X_train_scaled.reshape((-1,X_train_scaled.shape[0],X_train_scaled.shape[1]))
+X_crossValidate = np.reshape(X_crossValidate, (X_crossValidate.shape[0], X_crossValidate.shape[1],16))
+
+Algorithm.lstm(X_train, Y_train, X_crossValidate, Y_crossVal)
 
 
